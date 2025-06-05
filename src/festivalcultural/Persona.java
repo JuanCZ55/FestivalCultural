8e707b24422a0be34aec26cd4734618b247b0d21
Juan Cruz Zegarra <juancruzzegarra@gmail.com>
2025-06-05 13:34:36 -0300

    Se agregó la clase Persona con funcionalidad para gestionar talleres.

---
+++ src/festivalcultural/Persona.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package festivalcultural;
+
+import java.util.HashSet;
+
+/**
+ *
+ * @author juancz55
+ */
+public class Persona {
+
+    private HashSet<Taller> talleres = new HashSet<>();
+    private String nombre;
+
+    public Persona(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public HashSet<Taller> getTalleres() {
+        return talleres;
+    }
+
+    public boolean addTaller(Taller taller) {
+        return this.talleres.add(taller);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+}
