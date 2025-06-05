24bc436c663242edfa1d771dce161aad9ad42870
Juan Cruz Zegarra <juancruzzegarra@gmail.com>
2025-06-05 13:34:59 -0300

    Se agregó la clase Taller para la gestión de talleres en el festival cultural.

---
+++ src/festivalcultural/Taller.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package festivalcultural;
+
+import java.time.LocalTime;
+
+/**
+ *
+ * @author juancz55
+ */
+public class Taller {
+    private String nombre;
+    private LocalTime horaInicio,horaFin;
+    private String lugar;
+
+    public Taller(String nombre, LocalTime horaInicio, LocalTime horaFin, String lugar) {
+        this.nombre = nombre;
+        this.horaInicio = horaInicio;
+        this.horaFin = horaFin;
+        this.lugar = lugar;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalTime getHoraInicio() {
+        return horaInicio;
+    }
+
+    public void setHoraInicio(LocalTime horaInicio) {
+        this.horaInicio = horaInicio;
+    }
+
+    public LocalTime getHoraFin() {
+        return horaFin;
+    }
+
+    public void setHoraFin(LocalTime horaFin) {
+        this.horaFin = horaFin;
+    }
+
+    public String getLugar() {
+        return lugar;
+    }
+
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
+    }
+    
+}
